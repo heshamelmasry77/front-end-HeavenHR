90f8806ac8fd241ea2ecaf8b37737d088e0a5dcf
hesham <heshamelmasry1992@gmail.com>
2020-03-10 11:57:35 +0200

    add filter by sex and stared

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -23,13 +23,15 @@ class FriendsTableBody extends Component {
       currentPage: 0,
       sliceData: [],
       pageCount: 0,
-      search: []
+      search: [],
+      filteredFriends: [],
+      selectedFilter: null,
+      filterApplied: false
     };
     this.handlePageClick = this
       .handlePageClick
       .bind(this);
     this.searchHandler = this.searchHandler.bind(this)
-
   }
 
   handlePageClick = (e) => {
@@ -41,6 +43,7 @@ class FriendsTableBody extends Component {
       currentPage: selectedPage,
       offset: offset
     }, () => {
+
       const data = this.props.friends;
       const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
       console.log(slice)
@@ -50,21 +53,10 @@ class FriendsTableBody extends Component {
     });
   };
 
-  componentDidMount() {
-    console.log(this.props);
-    const data = this.props.friends;
-    const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
-    this.setState({
-      pageCount: Math.ceil(data.length / this.state.perPage),
-      sliceData: slice,
-    }, () => {
-      console.log(this.state)
-    })
-  }
-
   searchHandler(event) {
     console.log(event)
     if (event.target.value) {
+
       let searchQuery = event.target.value.toLowerCase();
       console.log('searchQuery:', searchQuery);
       console.log(this.state)
@@ -92,12 +84,76 @@ class FriendsTableBody extends Component {
     }
   }
 
+  componentDidMount() {
+    console.log(this.props);
+    const data = this.props.friends;
+    const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+    this.setState({
+      pageCount: Math.ceil(data.length / this.state.perPage),
+      sliceData: slice,
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
+  handleFilter(e) {
+    let filteredFriends = this.props.friends;
+    let friendFilter = e.target.value;
+
+    filteredFriends = filteredFriends.filter((friend) => {
+
+      // friend[friendFilter].toLowerCase().includes(friendFilter)
+      console.log(friendFilter)
+
+      switch (friendFilter) {
+        case 'male':
+          // code block
+          return friend.sex === 'male';
+        case 'female':
+          // code block
+          return friend.sex === 'female';
+        case 'stared':
+          // code block
+          return friend.isStared;
+        default:
+          return friend
+        // code block
+      }
+      // return friend[type] === friendFilter;
+    });
+    console.log(filteredFriends);
+    const slice = filteredFriends.slice(this.state.offset, this.state.offset + this.state.perPage)
+    this.setState({
+      pageCount: Math.ceil(filteredFriends.length / this.state.perPage),
+      sliceData: slice,
+    })
+
+  };
+
   render() {
-    let friends = this.state.sliceData
+    let friends = this.state.sliceData;
     return (
       <TableBody>
         <TableRow>
-          <input type="text" className="search" onChange={this.searchHandler}/> </TableRow>
+          <TableCell>
+            <div>
+              <select onChange={this.handleFilter.bind(this)}>
+                <option
+                  value="Choose Filter"
+                >Choose Filter
+                </option>
+                <option value="male">Male</option>
+                <option value="female">Female</option>
+                <option value="stared">Stared</option>
+              </select>
+            </div>
+          </TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell>
+            <input type="text" className="search" placeholder='Search by Name:' onChange={this.searchHandler}/>
+          </TableCell>
+        </TableRow>
         {
           friends.map(({id, name, sex, isStared}) => (
             <TableRow key={id}>
